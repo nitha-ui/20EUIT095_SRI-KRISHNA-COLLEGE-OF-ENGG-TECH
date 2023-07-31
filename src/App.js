869426e5ee15f2e6bb9d7e547227b0eed7cc51b8
nitha-ui <122962325+nitha-ui@users.noreply.github.com>
2023-07-31 21:16:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+
+function App() {
+
+// eslint-disable-next-line no-unused-vars
+const [backendData, setBackendData] = useState([{}])
+
+useEffect(() => {
+
+fetch("/api").then(
+
+response => response.json()
+
+       ).then
+     (
+
+         data=> {
+
+      setBackendData(data)
+   }
+  )
+}, [])
+
+return (
+
+  <div>
+  
+  {(typeof backendData.users === 'undefined') ? ( 
+     <p>Loading...</p>
+   ): (
+  
+  backendData.users.map((user, i) => ( 
+    <p key={i}>{user}</p>
+  ))
+   )}
+  
+  </div>
+)
+}
+
+export default App
+
+
+
